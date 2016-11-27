35bf04d1582ef9b6186487d864c740e5ecdd4ab4
Nguyenkain <nguyenkain@gmail.com>
2016-11-27 12:06:36 +0700

    Update config.php

--- dmlmap/config.php
+++ dmlmap/config.php
@@ -1,9 +1,9 @@
 <?php
 define("MAP_API", 'AIzaSyBgqaIuAZeUgkbaM-fH3wCc3uYyx2U4oKg');
 define("DB_HOST", 'localhost');
-define("DB_NAME", 'vov');
-define("DB_USER", 'root');
-define("DB_PASS", '123123123');
+define("DB_NAME", 'heroku_ed6f8d45e7e9f5e');
+define("DB_USER", 'b7feb475b236cd');
+define("DB_PASS", 'acdefa00');
 define("DEFAULT_LAT", '21.027764');
 define("DEFAULT_LONG", '105.834160');
 define("DEFAULT_ZOOM", '20');
